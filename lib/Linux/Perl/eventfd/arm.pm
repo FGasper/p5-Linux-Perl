e6623079fc7a938c4d00fc35aba0c3e774da6e45
Felipe Gasper <felipe@felipegasper.com>
2018-05-27 14:22:45 -0400

    cleanup?

--- lib/Linux/Perl/eventfd/arm.pm
+++ lib/Linux/Perl/eventfd/arm.pm
@@ -5,14 +5,9 @@ use warnings;
 
 use parent 'Linux::Perl::eventfd';
 
-use Linux::Perl::Constants::arm;
-
 use constant {
     NR_eventfd  => 351,
     NR_eventfd2 => 356,
 };
-
-*flag_CLOEXEC = *Linux::Perl::Constants::arm::flag_CLOEXEC;
-*flag_NONBLOCK = *Linux::Perl::Constants::arm::flag_NONBLOCK;
 
 1;
--- lib/Linux/Perl/eventfd/x86_64.pm
+++ lib/Linux/Perl/eventfd/x86_64.pm
@@ -5,8 +5,6 @@ use warnings;
 
 use parent 'Linux::Perl::eventfd';
 
-use Linux::Perl::Constants::x86_64;
-
 use constant {
     NR_eventfd  => 284,
     NR_eventfd2 => 290,
