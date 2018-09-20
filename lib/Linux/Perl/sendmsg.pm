64e75fed6317e92bc657ab33b18f00114ea95c77
Felipe Gasper <felipe@felipegasper.com>
2018-09-20 02:17:20 -0400

    POD

--- lib/Linux/Perl/sendmsg.pm
+++ lib/Linux/Perl/sendmsg.pm
@@ -1,19 +1,34 @@
 package Linux::Perl::sendmsg;
 
+use strict;
+use warnings;
+
 =encoding utf-8
 
 =head1 NAME
 
-Linux::Perl:sendmsg:
+Linux::Perl::sendmsg
 
 =head1 SYNOPSIS
 
+    my $bytes = Linux::Perl::sendmsg->sendmsg(
+        fd => $fd,
+        name => $name,
+        iovec => [ \$str1, \$str2, .. ],
+        control => [ $level, $type, $data ],
+        flags => \@flags,
+    );
+
+You can alternatively use your platform-specific module, e.g.,
+L<Linux::Perl::sendmsg::x86_64>.
+
 =head1 DESCRIPTION
 
-=cut
+This module provides a Linux-specific C<sendmsg()> implementation.
+See C<man 2 sendmsg> for what this can do differently from C<send()>
+and C<sendto>.
 
-use strict;
-use warnings;
+=cut
 
 use parent 'Linux::Perl::Base';
 
@@ -42,6 +57,7 @@ use constant {
     _iovec => q< P L! >,
 
     _sizelen => length( pack 'L!' ),
+    _intlen  => length( pack 'I!' ),
 
     _flag_CONFIRM   => 0x800,
     _flag_DONTROUTE => 4,
@@ -51,6 +67,31 @@ use constant {
     _flag_NOSIGNAL => 0x4000,
     _flag_OOB => 1,
 };
+
+=head1 METHODS
+
+=head2 I<CLASS>->sendmsg( %OPTS )
+
+%OPTS correspond to the system call:
+
+=over
+
+=item * C<fd>
+
+=item * C<name> - irrelevant for connected sockets
+
+=item * C<iovec> - Optional, a reference to an array of string references
+
+=item * C<control> - Optional, a reference to an array of: $LEVEL, $TYPE, $DATA.
+See below for examples. If you don’t use this, you might as well use Perl’s
+C<send()> built-in.
+
+=item * C<flags> - Optional, a reference to an array of any/all of: C<CONFIRM>,
+C<DONTROUTE>, C<DONTWAIT>, C<EOR>, C<MORE>, C<NOSIGNAL>, C<OOB>.
+
+=back
+
+=cut
 
 # fd, flags, name, iov, control(level, type, data)
 sub sendmsg {
@@ -73,7 +114,7 @@ sub sendmsg {
 
     my $control = $opts{'control'} && pack(
         _cmsghdr(),
-        16 + length $opts{'control'}[2],
+        _sizelen() + 2 * _intlen() + length $opts{'control'}[2],
         @{ $opts{'control'} },
     );
 
@@ -100,5 +141,34 @@ sub sendmsg {
         0 + $flags,
     );
 }
+
+#----------------------------------------------------------------------
+
+=head1 CONTROL EXAMPLES
+
+=head2 Sending credentials via local socket
+
+    use Socket;
+
+    control => [
+        Socket::SOL_SOCKET(), Socket::SCM_CREDENTIALS(),
+        pack( 'I!*', $$, $>, split( m< >, $) ) ),
+    ]
+
+=head2 Passing open file descriptors via local socket
+
+    control => [
+        Socket::SOL_SOCKET(), Socket::SCM_RIGHTS(),
+        pack( 'I!*', @file_descriptors ),
+    ]
+
+Also see L<Socket::MsgHdr>’s documentation for another example.
+
+=head1 TODO
+
+I’m not sure if C<recvmsg()> is feasible to implement in pure Perl,
+but that would be a natural complement to this module.
+
+=cut
 
 1;
